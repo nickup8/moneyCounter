494858ab1b3e4cf50ca18b6e16c2811809be977d
nickup8 <#Pozharskogo8>
2022-08-07 12:16:44 +0300

    Test merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Sidebar } from './components/Sidebar';
 import { TopMenu } from './components/TopMenu';
 
+import style from './App.module.scss';
+
 function App() {
 	return (
 		<div className="App">
 			<Sidebar />
 			<TopMenu />
+			<div className={style.content}></div>
 		</div>
 	);
 }
---
+++ src/pages/Costs/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Costs = () => {
+	return <div>Costs</div>;
+};
