14641e8377c2097f620b46ddcb3273aaf6ed5ea8
nickup8 <#Pozharskogo8>
2022-08-07 16:00:04 +0300

    Create component Input

--- src/components/ui/Input/index.jsx
+++ src/components/ui/Input/index.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import style from './Input.module.scss';
 
-export const Input = () => {
+export const Input = ({ type, label, placeholder, icon }) => {
 	return (
 		<div className={style.inputBox}>
-			<label className={style.label}>Ghbdtn</label>
+			<input
+				className={icon ? style.input + ' ' + style.inputIcon : style.input}
+				type={type}
+				placeholder={placeholder}
+			/>
+			<label className={style.label}>{label}</label>
+			{icon ? <img src={icon} className={style.icon} alt="icon" /> : ''}
 		</div>
 	);
 };
