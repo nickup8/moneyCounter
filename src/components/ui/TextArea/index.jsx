324a90a9de6ceef2c5897a29a27e6326674d5218
nickup8 <#Pozharskogo8>
2022-08-13 11:06:54 +0300

    Create component TextArea

--- src/components/ui/TextArea/index.jsx
+++ src/components/ui/TextArea/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import style from './textArea.module.scss';
 
 export const TextArea = () => {
-	return <div>TextArea</div>;
+	const [textValue, setTextValue] = React.useState('');
+
+	return (
+		<div className={style.textAreaBox}>
+			<label className={style.label}>Комментарий</label>
+			<textarea
+				rows="3"
+				className={style.textarea}
+				value={textValue}
+				onChange={(event) => setTextValue(event.target.value)}
+				placeholder="Краткое описание покупки"
+			></textarea>
+		</div>
+	);
 };
